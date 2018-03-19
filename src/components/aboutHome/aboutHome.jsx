d56d438e5ef6aedbe922290d48cd4c4fad72f697
roinuj16 <junior.si16@gmail.com>
2018-03-19 00:17:27 -0300

    comunicando com servidor backend

--- src/components/aboutHome/aboutHome.jsx
+++ src/components/aboutHome/aboutHome.jsx
@@ -1,46 +1,97 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-export default props => (
-    <section id="about" className="wow fadeIn ptb">
-        <div className="container">
-            <div className="row text-center">
-                <div className="col-md-8 col-md-offset-2">
-                    <h2>Por que escolher a Policlínica Saúde?</h2>
-                    <p className="lead">Todos os nossos profissionais são altamente qualificados, desde o atendimento inicial até os consultórios médicos.</p>
-                </div>
-            </div>
-            <div className="row why-choose mt-30">
-                <div className="col-md-3 col-sm-6 text-center">
-                    <div className="padding-20">
-                        <div className="page-icon-top"><i className="fa fa-user-md fa-4x"></i></div>
-                        <h5>Médicos</h5>
-                        <p>Profissionais capacitados e comprometidos com a saúde e o bem estar dos nossos pacientes.</p>
+const URL = 'http://localhost:8000/api/homeInfo';
+
+export default class AboutHome extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            titulo: '',
+            descricao: '',
+            icone1: '',
+            icone2: '',
+            icone3: '',
+            icone4: '',
+            label1: '',
+            label2: '',
+            label3: '',
+            label4: '',
+            infolable1: '',
+            infolable2: '',
+            infolable3: '',
+            infolable4: '',
+
+        };
+
+        this.getData();
+    }
+
+    getData() {
+        axios.get(`${URL}`).then(resp => console.log(resp.data.dados));
+        axios.get(`${URL}`).then(resp => this.setState({
+            ...this.state,
+            titulo: resp.data.dados.titulo,
+            descricao: resp.data.dados.descricao,
+            icone1: resp.data.dados.classe_css_1,
+            icone2: resp.data.dados.classe_css_2,
+            icone3: resp.data.dados.classe_css_3,
+            icone4: resp.data.dados.classe_css_4,
+            label1: resp.data.dados.label_1,
+            label2: resp.data.dados.label_2,
+            label3: resp.data.dados.label_3,
+            label4: resp.data.dados.label_4,
+            infolable1: resp.data.dados.info_label_1,
+            infolable2: resp.data.dados.info_label_2,
+            infolable3: resp.data.dados.info_label_3,
+            infolable4: resp.data.dados.info_label_4,
+
+        }));
+    }
+
+    render () {
+        return (
+            <section id="about" className="wow fadeIn ptb">
+                <div className="container">
+                    <div className="row text-center">
+                        <div className="col-md-8 col-md-offset-2">
+                            <h2>{ this.state.titulo }</h2>
+                            <p className="lead">{ this.state.descricao }</p>
+                        </div>
+                    </div>
+                    <div className="row why-choose mt-30">
+                        <div className="col-md-3 col-sm-6 text-center">
+                            <div className="padding-20">
+                                <div className="page-icon-top"><i className={`fa ${this.state.icone1 } fa-4x`}></i></div>
+                                <h5>{ this.state.label1 }</h5>
+                                <p>{ this.state.infolable1 }</p>
+                            </div>
+                        </div>
+                        <div className="col-md-3 col-sm-6 text-center">
+                            <div className="padding-20">
+                                <div className="page-icon-top"><i className={`fa ${this.state.icone2 } fa-4x`}></i></div>
+                                <h5>{ this.state.label2 }</h5>
+                                <p>{ this.state.infolable2 }</p>
+                            </div>
+                        </div>
+                        <div className="col-md-3 col-sm-6  text-center">
+                            <div className="padding-20">
+                                <div className="page-icon-top"><i className={`fa ${this.state.icone3 } fa-4x`}></i></div>
+                                <h5>{ this.state.label3 }</h5>
+                                <p>{ this.state.infolable3 }</p>
+                            </div>
+                        </div>
+                        <div className="col-md-3 col-sm-6 text-center">
+                            <div className="padding-20">
+                                <div className="page-icon-top"><i className={`fa ${this.state.icone4 } fa-4x`}></i></div>
+                                <h5>{ this.state.label4 }</h5>
+                                <p>{ this.state.infolable4 }</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="col-md-3 col-sm-6 text-center">
-                    <div className="padding-20">
-                        <div className="page-icon-top"><i className="fa fa-book fa-4x"></i></div>
-                        <h5>Cursos</h5>
-                        <p>
-                            Oferecemos cursos e treinamentos completos e com foco no mercado de trabalho.
-                        </p>
-                    </div>
-                </div>
-                <div className="col-md-3 col-sm-6  text-center">
-                    <div className="padding-20">
-                        <div className="page-icon-top"><i className="fa fa-headphones fa-4x"></i></div>
-                        <h5>Atendimento</h5>
-                        <p>Fazemos do processo de atendimento uma atividade de excelência para nossos pacientes.</p>
-                    </div>
-                </div>
-                <div className="col-md-3 col-sm-6 text-center">
-                    <div className="padding-20">
-                        <div className="page-icon-top"><i className="far fa-money-bill-alt fa-4x"></i></div>
-                        <h5>Praticidade </h5>
-                        <p>Consulta e exames com alto padrão de qualidade e com preços acessíveis.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-)
+            </section>
+        )
+     }
+}
