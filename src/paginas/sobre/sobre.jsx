14a66e302a0c292cce6eb84afd7d8bc99fe78510
roinuj16 <junior.si16@gmail.com>
2018-04-08 23:12:27 -0300

    integração da pagina quem somos com backend

--- src/paginas/sobre/sobre.jsx
+++ src/paginas/sobre/sobre.jsx
@@ -1,9 +1,32 @@
 import React, { Component } from 'react'
-import image from  '../../assets/images/image5.jpg'
+import axios from 'axios'
+import { myConfig } from '../../main/consts'
 
 import Breadcrumb from '../../components/breadcrumb/breadcrumb'
 
 export default class Sobre extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { titulo: '', conteudo: '', img_path: '' }
+    }
+
+    componentDidMount() {
+        axios.get(`${myConfig.apiUrl}/quem-somos`)
+            .then(response => {
+                console.log(response);
+                this.setState({
+                    ...this.state,
+                    titulo: response.data.dados.titulo,
+                    conteudo: response.data.dados.conteudo,
+                    img_path: response.data.dados.img_path,
+                })
+            })
+            .catch(function (error) {
+                console.log('erro:', error);
+            })
+    }
+
     render() {
         return (
             <div>
@@ -11,18 +34,10 @@ export default class Sobre extends Component {
                 <section className="about-section ptb">
                     <div className="container">
                         <div className="row">
-                            <div className="col-sm-6 pb-xs-30"> <img src={image} alt="" /> </div>
+                            <div className="col-sm-6 pb-xs-30"> <img src={this.state.img_path} alt="" /> </div>
                             <div className="col-sm-6"> <span className="sub-title">Policlínica Saúde & Vida</span>
-                                <h2>Consulta e exames com alto padrão de qualidade e com preços acessíveis.</h2>
-                                <p>
-                                    A Policlínica Saúde & Vida é um centro médico completo, que oferece aos pacientes diversas especialidades médicas. Buscando sempre a inovação e a qualidade na prestação de serviço a policlínica vêm se destacando com o preço justo em consultas e exames de imagens.
-                                </p>
-                                <p>
-                                    Responsabilidade e atendimento humanitário são os princípios da Policlínica Saúde & Vida, que enxerga em cada paciente um ser humano merecedor de atenção, cuidados e do melhor atendimento por parte de toda a equipe da clínica.
-                                </p>
-                                <p>
-                                    Todos os nossos profissionais são altamente qualificados, desde o atendimento inicial até os consultórios médicos. Hoje contamos com mais de 15 especialidade médicas, oferecendo todo o conforte e qualidade aos nossos clientes.
-                                </p>
+                                <h2>{ this.state.titulo }</h2>
+                                <div dangerouslySetInnerHTML={{__html: this.state.conteudo}}/>
                             </div>
                         </div>
                     </div>
